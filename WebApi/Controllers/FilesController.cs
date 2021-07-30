a63ada9d92f149fc9fc8b65504c9af1bab3733e8
avinashdhkl <abinashdhkl96@gmail.com>
2021-07-30 20:20:05 +0545

    first commit

---
+++ WebApi/Controllers/FilesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+       
+    }
+}
